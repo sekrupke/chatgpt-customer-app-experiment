0003c7c9209868f0f743b3c2587f43422edb6217
Sebastian Krupke <sebastian.krupke@ewe.de>
2023-05-15 11:53:34 +0200

    Added missing getters and setters for Entity

--- customer-app/src/main/java/de/chatgpt/sample/customerapp/models/Customer.java
+++ customer-app/src/main/java/de/chatgpt/sample/customerapp/models/Customer.java
@@ -8,17 +8,18 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Entity
 @Table(name = "customers")
 public class Customer {
+
     @Id
     private int id;
-    
+
     private String salutation;
     private String title;
     private String firstName;
     private String lastName;
-    
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
-    
+
     private String street;
     private String houseNumber;
     private String postalCode;
@@ -28,6 +29,126 @@ public class Customer {
     private String fax;
     private String email;
     private boolean newsletter;
-    
-    // Constructors, getters, and setters omitted for brevity
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isNewsletter() {
+        return newsletter;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNewsletter(boolean newsletter) {
+        this.newsletter = newsletter;
+    }
 }
